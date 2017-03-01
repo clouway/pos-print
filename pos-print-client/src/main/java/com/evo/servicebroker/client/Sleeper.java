741cd0deae3f57e620ebefab8f5e4e5c7868b904
Miroslav Genov <mgenov@gmail.com>
2017-03-01 14:55:35 +0200

    pos-print: erase old code which is not going to be used anymore
    The old code which were used in the past is no longer required and is
    deleted.

--- pos-print-client/src/main/java/com/evo/servicebroker/client/Sleeper.java
+++
@@ -1,8 +0,0 @@
-package com.evo.servicebroker.client;
-
-/**
- * @author Lazo Apostolovski (lazo.apostolovski@gmail.com)
- */
-interface Sleeper {
-  void sleep(Integer time);
-}
---
+++ pos-print/src/test/java/com/clouway/pos/print/ExampleTest.java
@@ -0,0 +1,7 @@
+package com.clouway.pos.print;
+
+/**
+ * @author Miroslav Genov (miroslav.genov@clouway.com)
+ */
+public class ExampleTest {
+}
