9720a435570a26f80d770e7e99bae34613138d0a
Borislav Gadjev <gadjevb@gmail.com>
2017-04-13 14:30:01 +0300

    pos-print: implemented datastore
    Added persistent datastore implementation for cash register devices, thus allowing to save devices indefinitely.
    
    Fixes #18

---
+++ pos-print/src/main/java/com/clouway/pos/print/persistent/PersistentCashRegisterRepository.kt
@@ -0,0 +1,59 @@
+package com.clouway.pos.print.persistent
+
+import com.clouway.pos.print.core.CashRegister
+import com.google.common.collect.Lists
+import com.google.inject.Inject
+import com.google.inject.Provider
+import com.mongodb.client.MongoCollection
+import com.mongodb.client.MongoDatabase
+import org.bson.Document
+
+/**
+ *@author Borislav Gadjev <borislav.gadjev@clouway.com>
+ */
+class PersistentCashRegisterRepository @Inject constructor(private val database: Provider<MongoDatabase>) : CashRegisterRepository {
+
+  override fun register(record: CashRegister): String {
+    val result = devices().find(Document().append("sourceIp", record.sourceIp))
+
+    if (result.first() != null) {
+      throw DeviceAlreadyExistException()
+    }
+
+    val query = adapt(record)
+    devices().insertOne(query)
+
+    return record.destination
+  }
+
+
+  override fun getAll(): List<CashRegister> {
+    var cashRegisters = Lists.newArrayList<CashRegister>()
+    var cursor = devices().find()
+
+    cursor.forEach {
+      cashRegisters.add(adapt(it))
+    }
+
+    return cashRegisters
+  }
+
+  private fun adapt(record: Document): CashRegister {
+    return CashRegister(
+      record.getString("sourceIp"),
+      record.getString("destination"),
+      record.getString("description")
+    )
+  }
+
+  private fun adapt(record: CashRegister): Document {
+    return Document()
+      .append("sourceIp", record.sourceIp)
+      .append("destination", record.destination)
+      .append("description", record.description)
+  }
+
+  private fun devices(): MongoCollection<Document> {
+    return database.get().getCollection("devices")
+  }
+}
